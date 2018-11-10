5027b4064612e3130072f10bad6d0ac591e7fc58
Josh <NGUTustin@gmail.com>
2018-11-09 20:48:08 -0600

    webBrowser fallback

--- PlayStationSharp/Forms/LoginForm.Designer.cs
+++ PlayStationSharp/Forms/LoginForm.Designer.cs
@@ -28,41 +28,23 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
-			this.webLogin = new Microsoft.Toolkit.Win32.UI.Controls.WinForms.WebView();
-			((System.ComponentModel.ISupportInitialize)(this.webLogin)).BeginInit();
 			this.SuspendLayout();
-			// 
-			// webLogin
-			// 
-			this.webLogin.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.webLogin.Location = new System.Drawing.Point(0, 0);
-			this.webLogin.MinimumSize = new System.Drawing.Size(20, 20);
-			this.webLogin.Name = "webLogin";
-			this.webLogin.Size = new System.Drawing.Size(574, 636);
-			this.webLogin.Source = new System.Uri(resources.GetString("webLogin.Source"), System.UriKind.Absolute);
-			this.webLogin.TabIndex = 0;
-			this.webLogin.NavigationStarting += new System.EventHandler<Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlNavigationStartingEventArgs>(this.webLogin_NavigationStarting);
 			// 
 			// LoginForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(574, 636);
-			this.Controls.Add(this.webLogin);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "LoginForm";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Login";
-			((System.ComponentModel.ISupportInitialize)(this.webLogin)).EndInit();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
-
-		private Microsoft.Toolkit.Win32.UI.Controls.WinForms.WebView webLogin;
 	}
 }
--- PlayStationSharp/Forms/LoginForm.cs
+++ PlayStationSharp/Forms/LoginForm.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Windows.Forms;
 using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
+using Microsoft.Toolkit.Win32.UI.Controls.WinForms;
 
 namespace PlayStationSharp.Forms
 {
@@ -9,16 +10,68 @@ namespace PlayStationSharp.Forms
 	{
 		public string GrantCode { get; private set; }
 
+		private string _loginUrl => "https://id.sonyentertainmentnetwork.com/signin/?service_entity=urn:service-entity:psn&response_type=code&client_id=ebee17ac-99fd-487c-9b1e-18ef50c39ab5&redirect_uri=com.playstation.PlayStationApp://redirect&scope=kamaji%3Aget_players_met%20kamaji%3Aget_account_hash%20kamaji%3Aactivity_feed_submit_feed_story%20kamaji%3Aactivity_feed_internal_feed_submit_story%20kamaji%3Aactivity_feed_get_news_feed%20kamaji%3Acommunities%20kamaji%3Agame_list%20kamaji%3Augc%3Adistributor%20oauth%3Amanage_device_usercodes%20psn%3Asceapp%20user%3Aaccount.profile.get%20user%3Aaccount.attributes.validate%20user%3Aaccount.settings.privacy.get%20kamaji%3Aactivity_feed_set_feed_privacy%20kamaji%3Asatchel%20kamaji%3Asatchel_delete%20user%3Aaccount.profile.update&request_locale=en_US";
+
+		// Okay, so the deal with this stuff is for some reason, the WebView control throws an exception for certain people.
+		// Since I couldn't repro the issue, the library will fallback to the built in .NET webBrowser control and use that instead.
+		// It's pretty horrible, but it'll have to do until I can find out why the WebView crashes.
 		public LoginForm()
 		{
 			InitializeComponent();
+			try
+			{
+				var webViewLogin = new WebView
+				{
+					Dock = System.Windows.Forms.DockStyle.Fill,
+					Location = new System.Drawing.Point(0, 0),
+					MinimumSize = new System.Drawing.Size(20, 20),
+					Name = "webLogin",
+					Size = new System.Drawing.Size(574, 636),
+					Source = new System.Uri(this._loginUrl, System.UriKind.Absolute),
+					TabIndex = 0,
+					Visible = true
+				};
+				webViewLogin.NavigationStarting += webViewLogin_NavigationStarting;
+
+				this.Controls.Add(webViewLogin);
+			}
+			catch (Exception)
+			{
+				var webBrowserLogin = new WebBrowser
+				{
+					Dock = System.Windows.Forms.DockStyle.Fill,
+					Location = new System.Drawing.Point(0, 0),
+					MinimumSize = new System.Drawing.Size(20, 20),
+					Name = "webLogin",
+					Size = new System.Drawing.Size(574, 636),
+					Url = new System.Uri(this._loginUrl, System.UriKind.Absolute),
+					TabIndex = 0,
+					Visible = true
+				};
+
+				webBrowserLogin.Navigated += webBrowserLogin_Navigated;
+
+				this.Controls.Add(webBrowserLogin);
+			}
 		}
 
-		private void webLogin_NavigationStarting(object sender, WebViewControlNavigationStartingEventArgs e)
+		private void webViewLogin_NavigationStarting(object sender, WebViewControlNavigationStartingEventArgs e)
 		{
 			var url = Uri.UnescapeDataString(e.Uri.ToString());
 
 			if (!url.StartsWith("https://auth.api.sonyentertainmentnetwork.com/mobile-success.jsp")) return;
+
+			var paramName = "code=";
+			var code = url.Remove(0, url.IndexOf(paramName, StringComparison.Ordinal) + paramName.Length);
+			GrantCode = code.Substring(0, code.IndexOf("&", StringComparison.Ordinal));
+			this.Close();
+		}
+
+		private void webBrowserLogin_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+		{
+			var url = Uri.UnescapeDataString(e.Url.ToString());
+
+			if (!url.ToString().StartsWith("https://auth.api.sonyentertainmentnetwork.com/mobile-success.jsp")) return;
 
 			var paramName = "code=";
 			var code = url.Remove(0, url.IndexOf(paramName, StringComparison.Ordinal) + paramName.Length);
