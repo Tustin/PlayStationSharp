24b9db21bdbd947a9eb6714e3cc60af3aeb24d1d
Beckam White <47697544+BJDubb@users.noreply.github.com>
2020-05-15 22:00:11 +1000

    Update PlayStationApiException.cs
    Allows the underlying error to be thrown when debugging. Errors such as "(2114059) Rate limit exceeded" aren't visible.

--- PlayStationSharp/Exceptions/PlayStationApiException.cs
+++ PlayStationSharp/Exceptions/PlayStationApiException.cs
@@ -8,6 +8,9 @@ namespace PlayStationSharp.Exceptions
 	{
 		public Error Error;
 
-		public PlayStationApiException(Error error) => Error = error;
+		public PlayStationApiException(Error error) : base(error.Message)
+        {
+            Error = error;
+        }
 	}
 }
